b423bf0fdc71cb823fdae5e6c8bf7ad3db5a02a8
SreyNeang <HP@DESKTOP-NUQJ5OT>
2025-07-23 06:26:48 -0700

    Homework2

---
+++ MyFirstMvcApp/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("This is the book index page.");
+        }
+        // Do the same for the other two methods 
+        public IActionResult Details(int id)
+        {
+            // For now, just display the ID that was passed in. 
+            return Content($"Showing details for book with ID: {id}");
+        }
+        public IActionResult All()
+        {
+            return Content("<h1>Here is a list of ALL books!</h1>", "text/html");
+        }
+        public IActionResult OldCatalog()
+        {
+            //​​ Thistells the browser to make a new request to the "/Books/All" URL. 
+            return RedirectToAction("All", "Books");
+        }
+    }
+}
